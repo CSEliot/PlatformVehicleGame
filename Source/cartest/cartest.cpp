c5acc08efcfcd063b1160d64be453ad947095ee0
conradte3 <conradte3@gmail.com>
2016-03-28 20:14:45 -0400

    Removed unnecessary source, added platform spawning blueprints and test level

--- Source/cartest/cartest.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "cartest.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cartest, "cartest" );
--- Source/jumpercables/jumpercables.h
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __jumpercablesADV_H__
-#define __jumpercablesADV_H__
-
-#include "EngineMinimal.h"
-
-#endif
